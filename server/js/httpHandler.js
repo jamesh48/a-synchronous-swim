58322bf6ccc40f3cc7b7225bd4aa2e985bbd36c7
Ernest Tan <ernesttancw@gmail.com>
2020-12-17 13:33:31 +0800

    Complete connecting client to server

--- server/js/httpHandler.js
+++ server/js/httpHandler.js
@@ -13,12 +13,13 @@ module.exports.initialize = (queue) => {
 };
 
 module.exports.router = (req, res, next = ()=>{}) => {
-  console.log('Serving request type ' + req.method + ' for url ' + req.url);
-  if (req.method === 'OPTIONS' && req.url === '/') {
+  console.log('Serving request type ' + req.method + ' for urlx ' + req.url);
+  if (req.method === 'OPTIONS') {
     res.writeHead(200, headers);
     res.end();
-  } else if (req.method === 'GET' && req.url === '/') {
+  } else if (req.method === 'GET') {
     // Generate random swim command
+    console.log('test here');
     let directions = ['left', 'right', 'up', 'down'];
     let randomDir = directions[Math.floor(Math.random() * directions.length)];
     // Send swim command to response request
